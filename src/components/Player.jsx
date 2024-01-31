49dc30a58dc7d2b9855913064eba181b3eab3ef6
murty-555 <krlvmurthy96@gmail.com>
2024-01-31 13:00:42 +0530

    added state depending on it we are using condintional rendering to display player name and input

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,12 +1,17 @@
-
+import {useState} from 'react';
 export default function Player({name, symbol}){
+    const [isEditing, setIsEditing] = useState(false);
+    function handleClick(){
+        setIsEditing(!isEditing)
+    }
     return (
         <li>
             <span className="player">
-              <span className="player-name">{name}</span>
+              {!isEditing && <span className="player-name">{name}</span>}
+              {isEditing && <input type='text' placeholder='Enter' value={name}/>}
               <span className="player-symbol">{symbol}</span>
             </span>
-            <button>Edit</button>
+            <button onClick={handleClick}>{!isEditing ? 'Edit' : 'Save'}</button>
           </li>
     )
 }
