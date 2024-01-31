359fd7b72162ea60ddde3e3d4c63216e7c0aa406
murty-555 <krlvmurthy96@gmail.com>
2024-01-31 11:14:43 +0530

    created players list in game container

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
-    <div>
-      <p>Main content coming soon...</p>
-    </div>
+    <main>
+      <div id="game-container">
+        {/* PLAYERS */}
+        <ol id="players">
+          <li>
+            <span className="player">
+              <span className="player-name">Player 1</span>
+              <span className="player-symbol">X</span>
+            </span>
+            <button>Edit</button>
+          </li>
+          <li>
+            <span className="player">
+              <span className="player-name">Player 2</span>
+              <span className="player-symbol">O</span>
+            </span>
+            <button>Edit</button>
+          </li>
+        </ol>
+
+        {/* GAME BOARD */}
+      </div>
+      {/* LOG */}
+    </main>
   );
 }
 
