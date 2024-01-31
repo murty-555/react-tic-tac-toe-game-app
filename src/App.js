7ccc6685a9f433979b4c2b17c180c006e2494dcc
murty-555 <krlvmurthy96@gmail.com>
2024-01-31 11:26:29 +0530

    replaced repeating part of players with single player component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import Player from "./components/Player";
 
 function App() {
   return (
@@ -6,20 +7,8 @@ function App() {
       <div id="game-container">
         {/* PLAYERS */}
         <ol id="players">
-          <li>
-            <span className="player">
-              <span className="player-name">Player 1</span>
-              <span className="player-symbol">X</span>
-            </span>
-            <button>Edit</button>
-          </li>
-          <li>
-            <span className="player">
-              <span className="player-name">Player 2</span>
-              <span className="player-symbol">O</span>
-            </span>
-            <button>Edit</button>
-          </li>
+          <Player name="Player 1" symbol="X"/>
+          <Player name="Player 2" symbol="O"/>
         </ol>
 
         {/* GAME BOARD */}
---
+++ src/components/Player.jsx
@@ -0,0 +1,12 @@
+
+export default function Player({name, symbol}){
+    return (
+        <li>
+            <span className="player">
+              <span className="player-name">{name}</span>
+              <span className="player-symbol">{symbol}</span>
+            </span>
+            <button>Edit</button>
+          </li>
+    )
+}
