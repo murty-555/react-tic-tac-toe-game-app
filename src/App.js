f5effc4b27ef61eded21bd9587d88b857549db57
murty-555 <krlvmurthy96@gmail.com>
2024-01-31 10:54:58 +0530

    placing fixed content into index.html which is independent of state

--- src/App.js
+++ src/App.js
@@ -3,10 +3,7 @@ import './App.css';
 function App() {
   return (
     <div>
-      <header>
-        <img src='game-logo.png' alt='hand drawn game logo'/>
-        <h1>Tic-Tac-Toe</h1>
-      </header>
+      <p>Main content coming soon...</p>
     </div>
   );
 }
