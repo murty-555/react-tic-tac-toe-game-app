e92b000532d2149738eebf55c968de00e1864c1c
murty-555 <krlvmurthy96@gmail.com>
2024-01-31 14:59:32 +0530

    user input and two way binding

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,19 +1,33 @@
-import {useState} from 'react';
-export default function Player({name, symbol}){
-    const [isEditing, setIsEditing] = useState(false);
-    function handleClick(){
-        // setIsEditing(!isEditing)
-        //Instead of updating the state as above, it is better to update like below
-        setIsEditing((editing) => !editing)
-    }
-    return (
-        <li>
-            <span className="player">
-              {!isEditing && <span className="player-name">{name}</span>}
-              {isEditing && <input type='text' placeholder='Enter' value={name}/>}
-              <span className="player-symbol">{symbol}</span>
-            </span>
-            <button onClick={handleClick}>{!isEditing ? 'Edit' : 'Save'}</button>
-          </li>
-    )
+import { useState } from "react";
+export default function Player({ initialName, symbol }) {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
+
+  function handleClick() {
+    // setIsEditing(!isEditing)
+    //Instead of updating the state as above, it is better to update like below
+    setIsEditing((editing) => !editing);
+  }
+
+  function handleChange(event) {
+    setPlayerName(event.target.value);
+  }
+
+  return (
+    <li>
+      <span className="player">
+        {!isEditing && <span className="player-name">{playerName}</span>}
+        {isEditing && (
+          <input
+            type="text"
+            placeholder="Enter"
+            value={playerName}
+            onChange={handleChange}
+          />
+        )}
+        <span className="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleClick}>{!isEditing ? "Edit" : "Save"}</button>
+    </li>
+  );
 }
