543e2451d5f0c90abbd5864352c1712bccd620e2
murty-555 <krlvmurthy96@gmail.com>
2024-02-01 11:23:44 +0530

    updating object state immutably and deep copy, shallow copy and structuredClone method

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const intialGameBoard = [
   [null, null, null],
   [null, null, null],
@@ -5,14 +7,31 @@ const intialGameBoard = [
 ];
 
 export default function GameBoard() {
+    const [gameBoard, setGameBoard] = useState(intialGameBoard)
+    
+    function handleSelectBox(rowIndex, colIndex){
+        setGameBoard((prevGameBoard) => {
+            //By doing this we can create deep copy of prevGameBoard
+            // const updatedGameBoard = [...prevGameBoard.map(innerArray => [...innerArray])]
+            
+            //By using this we can create a shallow copy of prevGameBoard
+            // const updatedGameBoard = [...prevGameBoard]
+
+            //The global structuredClone() method creates a deep clone of a given value
+            const updatedGameBoard = structuredClone(prevGameBoard)
+            updatedGameBoard[rowIndex][colIndex]="X"
+            return updatedGameBoard
+        })
+    }
+
   return (
     <ol id="game-board">
-      {intialGameBoard.map((row, rowIndex) => (
+      {gameBoard.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button>{playerSymbol}</button>
+                <button onClick={() => handleSelectBox(rowIndex, colIndex)}>{playerSymbol}</button>
               </li>
             ))}
           </ol>
